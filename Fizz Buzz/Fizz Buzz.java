5b891cc906a7480ca9815bf1137d87b0743c99e4
Heeyoung Yang <dev.heeyoung@gmail.com>
2021-11-09 12:55:53 -0500

    fixed solution

--- Fizz Buzz/Fizz Buzz.java
+++ Fizz Buzz/Fizz Buzz.java
@@ -1,17 +1,17 @@
 class Solution {
-   public List<String> fizzBuzz(int n){
+    public List<String> fizzBuzz(int n) {
         List<String> result = new ArrayList<String>(n);
-       
-       for(int i=0; i<n; i++){
-           if((i+1)%15==0)
-               result.add("FizzBuzz");
-           else if((i+1)%3==0) 
-               result.add("Fizz");
-           else if((i+1)%5==0)
-               result.add("Buzz");
-           else
-               result.add(String.valueOf(i+1));
-       }
-       return result;
-   }
+        
+        for(int i=1; i<=n; i++) {
+            if(i %15 == 0)
+                result.add("FizzBuzz");
+            else if(i%3 == 0)
+                result.add("Fizz");
+            else if(i%5 == 0)
+                result.add("Buzz");
+            else 
+                result.add(String.valueOf(i));
+        }
+        return result;
+    }
 }
