c52d50fb33040c7b9ab6f1bd29ae5854a3c724f3
Heeyoung Yang <dev.heeyoung@gmail.com>
2022-01-12 20:25:08 -0500

    Positions of Large Groups
    01/12/2022 Positions of Large Groups

---
+++ Positions of Large Groups/Positions of Large Groups.java
@@ -0,0 +1,12 @@
+class Solution {
+     public List<List<Integer>> largeGroupPositions(String S) {
+         List<List<Integer>> res = new ArrayList<>();
+         
+         for(int i=0, j=0; i<S.length(); i=j){
+             while(j < S.length() && S.charAt(i) == S.charAt(j)) j++;
+             if(j - i >= 3)
+                 res.add(Arrays.asList(i, j-1));
+         }
+         return res;
+    }
+}
---
+++ Positions of Large Groups/README.md
@@ -0,0 +1,7 @@
+# Question
+Positions of Large Groups
+
+# Source
+https://leetcode.com/problems/positions-of-large-groups/
+
+# Solution
