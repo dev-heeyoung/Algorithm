cff6ab8d86d7e9448290a296e7513af7efe62498
Heeyoung Yang <dev.heeyoung@gmail.com>
2022-07-24 14:04:24 -0400

    added solution
    07/24/2022 First Bad Version

---
+++ First Bad Version/First Bad Version.java
@@ -0,0 +1,15 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        
+        while (start<=end) {
+            int mid = start + (end-start)/2;
+            if (isBadVersion(mid)) {
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
---
+++ First Bad Version/README.md
@@ -0,0 +1,10 @@
+# Question
+First Bad Version
+
+# Source
+https://leetcode.com/problems/first-bad-version/
+
+# Solution
+ - Time Complexity: O(logN)
+ - Used binary search
+ - mid = start + (end-start)/2: To prevent overflow problem
