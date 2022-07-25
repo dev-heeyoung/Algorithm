187e7517198186bf88ffb3a480258f811dac2e06
Heeyoung Yang <dev.heeyoung@gmail.com>
2022-07-25 10:33:10 -0400

    added solution
    07/25/2022 Pascal's Triangle

---
+++ Pascal's Triangle/Pascal's Triangle.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        for (int i=0; i<numRows; i++) {
+            List<Integer> list = new ArrayList<>(); 
+            
+            for (int j=0; j<=i; j++){
+                if(j==0 || j==i) {
+                    list.add(1);
+                }
+                else {
+                    int a = result.get(i-1).get(j-1);
+                    int b = result.get(i-1).get(j);
+                    list.add(a+b);
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
